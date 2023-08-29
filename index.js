d76960d239943a1f2659a6e76905cbd10a886717
Kıvanç Güçkıran <k.guckiran@vispera.co>
2023-08-29 16:07:08 +0300

    use environment for port

--- index.js
+++ index.js
@@ -60,6 +60,6 @@ app.get("/", async (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log(`Example app listening on port ${3000}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
